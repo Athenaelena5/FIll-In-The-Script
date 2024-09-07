83eac34143b63544c395bde5c6dd05018fbd18e7
ankteaaa <ankt@eaaa.dk>
2024-09-07 17:08:03 +0200

    The Big Announcement

---
+++ Programmering 3/Forms - The Big Announcement/script.js
@@ -0,0 +1,11 @@
+const minForm = document.querySelector("form");
+const p_output = document.getElementById("p_output");
+const button = document.getElementById("send");
+
+let besked = document.getElementById("besked");
+
+button.onclick = visBesked;
+
+function visBesked(){
+    p_output.innerHTML = besked.value;
+}
