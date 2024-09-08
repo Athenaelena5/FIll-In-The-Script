6b7442902b5228dd3bafb743dd3602331725e4d6
ankteaaa <90596636+ankteaaa@users.noreply.github.com>
2024-09-08 21:13:54 +0200

    Togglebutton (nice) tilføjet

---
+++ Programmering 3/Togglebutton (nice)/script.js
@@ -0,0 +1,25 @@
+// VARIABLE:
+/*
+- Til knappen
+- Til menuen (vi skal vise og skjule den)
+
+- En booleanvariabel (let, ikke const) med navnet menuSynlig. Skal sættes til false til at starte med.
+
+*/
+
+// EVENT LISTENERS:
+/*
+- Knappen skal kunne klikkes og starte funktionen 'visMenu'
+*/
+
+// EVENT HANDLERS:
+/*
+- Funktionen visMenu:
+    Hvis menuSynlig == false, så 
+    - sæt animationName til "runIn"
+    - sæt menuSynlig til "true"
+    Ellers:
+    - sæt animationName til "runOut"
+    - sæt menuSynlig til "false"
+
+*/
