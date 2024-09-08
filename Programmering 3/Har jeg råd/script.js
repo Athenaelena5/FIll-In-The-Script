1d76f53f2baeb62bd66e6dab5463b19c3242a438
ankteaaa <90596636+ankteaaa@users.noreply.github.com>
2024-09-08 20:39:45 +0200

    Har jeg råd tilføjet

---
+++ Programmering 3/Har jeg råd/script.js
@@ -0,0 +1,18 @@
+// VARIABLE:
+/* 
+- Til knappen 'knap_beregn', brug querySelector
+- Til inputfeltet til budget
+- Til inputfeltet til pris
+*/
+
+// EVENT LISTENERS:
+/*
+- Gør knappen 'knap_beregn' klikbar, og aktiver funktionen 'beregn'
+*/
+
+// FUNCTIONS / EVENT HANDLERS:
+/*
+- Lav funktioen 'beregn'. 
+- Hvis værdien i prisfeltet er større end værdien i budgetfeltet --> popup der siger man ikke har råd.
+- Ellers: Popup, der siger man har råd.
+*/
