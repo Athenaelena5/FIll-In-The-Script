0c56d69217c4fa5b20ea7e23782a7042146e1490
Andreas Kure Thorngreen (lektor – ankt@eaaa.dk) <ankt@eaaa.dk>
2024-09-09 07:14:05 +0200

    Locked

---
+++ Programmering 3/Locked (mangler if-sætning)/script.js
@@ -0,0 +1,12 @@
+const input_pass = document.getElementById("input_pass");
+const btn_enter = document.getElementById("btn_enter");
+
+const venstreport = document.getElementById("venstreport");
+const hoejreport = document.getElementById("hoejreport");
+
+
+btn_enter.onclick = gaaInd;
+
+function gaaInd(){
+    /* Skriv en if-sætning, der spørger om værdien (value) af input_pass er lig med "locked" (en string, du kan også vælge et andet password :)). Hvis det er, så sæt animationName for venstreport til "udTilVenstre" og for hoejreport til "udTilHoejre". Ellers skal der bare vises en alert. */
+}
