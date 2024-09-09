dbb840602f5da3b2d28cb9f8087f1ebdb7f2850c
Athenaelena5 <160113211+Athenaelena5@users.noreply.github.com>
2024-09-09 10:23:51 +0200

    Update script.js

--- Programmering 3/Forms - The Big Announcement/script.js
+++ Programmering 3/Forms - The Big Announcement/script.js
@@ -1,12 +1,23 @@
 // VARIABLE:
 // - Til <p> så vi kan putte HTML ind i den. Kald den 'p_output'.
-// - Til knappen, så vi kan gøre den trykbar.
-// - Til inputfeltet, så vi kan hente indholdet af den.
+const p_output = document. getElementById("p_output");
 
+// - Til knappen, så vi kan gøre den trykbar.
+
+const btn_send = document.getElementById("send");
+
+// - Til inputfeltet, så vi kan hente indholdet af den.
+const input_besked = document.getElementById("besked");
 // EVENT LISTENER:
 // Knappen skal have en onclick event listener og kalde funktionen "visBesked"
 
+btn_send.onclick = visbesked;
+
 // FUNCTIONS / EVENT HANDLERS:
+function visbesked(){
+p_output.innerHTML = input_besked.value;
+}
+
 // Opret funktionen visBesked, der putter indholdet af inputfeltet ind i <p>
 // Du kan bruge denne kode:     
 //      p_output.innerHTML = besked.value;
